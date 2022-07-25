c37b64ef8e16c1f03e6c03708d1e8e34186593e9
Hannah Kim <devhkim13@gmail.com>
2022-07-25 09:26:48 -0700

    Add HTMLWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
     devtool: "inline-source-map",
+    plugins: [
+        new HtmlWebpackPlugin({
+          title: "Stacks Restaurant",
+        }),
+      ],
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
